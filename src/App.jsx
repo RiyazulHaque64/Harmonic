cd09472526a4da41fa2d36c624b29ee71f9b5357
RiyazulHaque64 <riyazulhaque64@gmail.com>
2023-06-06 23:18:53 +0600

    setup initial template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-green-500 text-5xl">This is harmonic</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
